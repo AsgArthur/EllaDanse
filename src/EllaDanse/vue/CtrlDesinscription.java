c7e60012da537dc4cbcfe43c0bb9a1df13dc0e3b
Sacha <sacha.durand971@gmail.com>
2025-06-11 13:46:03 +0100

    e

---
+++ src/EllaDanse/vue/CtrlDesinscription.java
@@ -0,0 +1,24 @@
+package EllaDanse.vue;
+
+import javafx.fxml.FXML;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class CtrlDesinscription {
+
+    @FXML public void valider() {
+
+    }
+
+    @FXML public void annuler(){
+
+    }
+
+
+
+
+
+    public void initialize() {
+    }
+}
