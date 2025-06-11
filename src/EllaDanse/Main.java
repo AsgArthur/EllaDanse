886775e1332ed3374f27c5d102b393b5f89473ae
NeirdaG <gadrieng@gmail.com>
2025-06-11 13:43:08 +0200

    caca

--- src/EllaDanse/Main.java
+++ src/EllaDanse/Main.java
@@ -1,13 +1,26 @@
 package EllaDanse;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+import javafx.application.Application;
+import javafx.stage.Stage;
+import modele.*;
+import vue.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main extends Application{
+
+    public void start(Stage f) throws Exception {
+
+        Donnees.chargementDonnees();
+        //initialisation des fenêtres
+        fNouvEmp = new FenNouvelEmploye();
+        fModifierEmp = new FenModifierEmploye();
+        fListeEmp = new FenListeEmployes();
+        fListeEmp.show();
     }
-}
+
+    static public void main(String args[]) {
+        Application.launch(args);
+    }
+
+    static public void fermerAppli() {
+
+    }
