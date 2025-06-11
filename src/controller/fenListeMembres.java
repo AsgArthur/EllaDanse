6a9f582fb1e759220c93454e9a06cfaeb6366cbe
AsgArthur <crenarthur@gmail.com>
2025-06-11 09:28:59 +0100

    suppresion

--- src/controller/fenListeMembres.java
+++ src/controller/fenListeMembres.java
@@ -1,5 +1,30 @@
 package controller;
 
-public class fenListeMembres {
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.IOException;
+
+public class FenListeMembres extends Stage {
+
+    private CtrlListeMembres ctrl;
+
+    public FenListeMembres() throws IOException {
+        this.setTitle("Accueil EllaDanse");
+        this.setResizable(false);
+        Scene laScene = new Scene(creerSceneGraph());
+        this.setScene(laScene);
+    }
+
+    private Pane creerSceneGraph() throws IOException {
+        File f = new File("src/EllaDanse/listeMembres.fxml");
+        FXMLLoader loader;
+        loader = new FXMLLoader(f.toURI().toURL());
+        Pane racine = loader.load();
+        ctrl = loader.getController();
+        return racine;
+    }
 }
