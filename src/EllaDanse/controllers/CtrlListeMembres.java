166a823bcefa25f715a65dcee339b8fdba5811d4
AsgArthur <crenarthur@gmail.com>
2025-06-11 12:56:06 +0100

    test

--- src/EllaDanse/controllers/CtrlListeMembres.java
+++ src/EllaDanse/controllers/CtrlListeMembres.java
@@ -1,5 +1,43 @@
 package EllaDanse.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class CtrlListeMembres {
+
+    @FXML private TableColumn<?, ?> ageCol;
+    @FXML private TableColumn<?, ?> coursCol;
+    @FXML private TableColumn<?, ?> emailCol;
+    @FXML private TableColumn<?, ?> idCol;
+    @FXML private TableView<?> membresTable;
+    @FXML private Button modifierBtn;
+    @FXML private TableColumn<?, ?> nomCol;
+    @FXML private TableColumn<?, ?> prenomCol;
+    @FXML private Button profilBtn;
+    @FXML private TextField rechercheField;
+    @FXML private TableColumn<?, ?> saisonCol;
+    @FXML private ComboBox<?> saisonComboBox;
+    @FXML private Button supprimerBtn;
+    @FXML private Label totalMembresLabel;
+    @FXML private ComboBox<?> triComboBox;
+
+
+    @FXML void modifierMembre(ActionEvent event) {
+
+    }
+
+    @FXML void ouvrirProfilMembre(ActionEvent event) {
+
+    }
+
+    @FXML void supprimerMembre(ActionEvent event) {
+
+    }
 
 }
