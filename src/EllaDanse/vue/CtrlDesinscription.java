93eacd1c34ae1554197409d7cc200468cf84f906
Sacha <sacha.durand971@gmail.com>
2025-06-11 14:06:47 +0100

    e

--- src/EllaDanse/vue/CtrlDesinscription.java
+++ src/EllaDanse/vue/CtrlDesinscription.java
@@ -2,23 +2,77 @@ package EllaDanse.vue;
 
 import javafx.fxml.FXML;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
 
 public class CtrlDesinscription {
 
-    @FXML public void valider() {
+
+    @FXML
+    private ListView<?> coursInscritsListView;
+
+    @FXML
+    private Button desinscrireBtn;
+
+    @FXML
+    private Button desinscrireTousBtn;
+
+    @FXML
+    private Button fermerBtn;
+
+    @FXML
+    private Label infoLabel;
+
+    @FXML
+    private ComboBox<?> membreComboBox;
+
+    @FXML
+    private Label messageLabel;
+
+    @FXML
+    private Button rechercherMembreBtn;
+
+    @FXML
+    private ComboBox<?> saisonComboBox;
+
+    @FXML
+    void annulerToutesInscriptions(ActionEvent event) {
 
     }
 
-    @FXML public void annuler(){
+    @FXML
+    void fermer(ActionEvent event) {
 
     }
 
+    @FXML
+    void handleDesinscrire(ActionEvent event) {
 
-
-
-
-    public void initialize() {
     }
+
+    @FXML
+    void onCoursSelected(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onMembreChanged(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSaisonChanged(ActionEvent event) {
+
+    }
+
+    @FXML
+    void rechercherMembre(ActionEvent event) {
+
+    }
+
 }
