25332c3a93f66af7f7f429ea75ef1122dc5d0f73
NeirdaG <gadrieng@gmail.com>
2025-06-11 10:13:23 +0200

    caca

---
+++ src/EllaDanse/FenAccueil.java
@@ -0,0 +1,31 @@
+package EllaDanse;
+
+import java.io.File;
+
+import java.io.IOException;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.layout.Pane;
+import javafx.fxml.FXMLLoader;
+
+
+public class FenAccueil extends Stage {
+
+    private CtrlAccueil ctrl;
+
+    public FenAccueil() throws IOException {
+        this.setTitle("Accueil EllaDanse");
+        this.setResizable(false);
+        Scene laScene = new Scene(creerSceneGraph());
+        this.setScene(laScene);
+    }
+
+    private Pane creerSceneGraph() throws IOException {
+        File f = new File("C:Users/gadri/Documents/cours/devApp/SAE/ellaDanse/src/acceuil.fxml");
+        FXMLLoader loader;
+        loader = new FXMLLoader(f.toURI().toURL());
+        Pane racine = loader.load();
+        ctrl = loader.getController();
+        return racine;
+    }
+}
--- src/EllaDanse/Main.java
+++ src/EllaDanse/Main.java
@@ -1,3 +1,5 @@
+package EllaDanse;
+
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
@@ -5,7 +7,7 @@ public class Main {
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);cccc
+            System.out.println("i = " + i);
         }
     }
 }
